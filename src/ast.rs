22a635e36063ae14f6719ee393465eba979c75b5
Matthew Hall <mdhall@gmail.com>
2016-07-25 20:17:13 -0500

    Getting parser to compile

---
+++ src/ast.rs
@@ -0,0 +1,32 @@
+
+pub enum Literal {
+  Float(f64),
+  Int(i64),
+  Bool(bool),
+  Str(String),
+}
+
+pub enum EscapeSeqs {
+    RightBrace,
+    LeftBrace,
+    Space,
+    NewLine,
+    CarriageReturn
+}
+
+pub enum Ast {
+  Block(Vec<Box<Ast>>),
+  Literal(Literal),
+  Identifier(Option<String>, Option<String>),
+  Format(String),
+  Buffer(String),
+  Escape(EscapeSeqs),
+  Raw(String),
+  FnCreate(Option<Vec<String>>, Box<Ast>), // TODO: can the 2nd, AST, be limited to block?
+  Pipe(Box<Ast>, Vec<Box<Ast>>),
+  Call(Box<Ast>, Vec<Box<Ast>>),
+
+  Map(Option<Vec<(String, Box<Ast>)>>),
+  Array(Option<Vec<Box<Ast>>>),
+  Empty
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+#![feature(plugin)]
+#![plugin(peg_syntax_ext)]
+
+mod ast;
+use ast::Literal;
+use ast::EscapeSeqs;
+use ast::Ast;
+
+peg_file! modname("lisplate.rustpeg");
+
 #[cfg(test)]
 mod tests {
     #[test]
